97383a4bc9c221bad94d6712a7d1365f5b4fd5d6
Ireshan Pathirana <Ireshan_107185@dialog.dialoggsm.com>
2020-01-27 09:55:29 +0530

    made async the library

--- Validation.js
+++ Validation.js
@@ -4,10 +4,10 @@ class Validation {
         this.object = object;
         // this.check(request, object);
     }
-    check() {
+    async check() {
         for (const property in this.object) {
             let check = this.object[property].split('|');
-            let errors = check.map(type => {
+            let errors = await Promise.all(check.map(type => {
                 if (type.includes(':')) {
                     let [func, amount] = type.split(':');
                     return this[func](amount, this.request[property]);
@@ -15,48 +15,48 @@ class Validation {
                 if (!type.includes(':')) {
                     return this[type](this.request[property], property);
                 }
-            });
+            }));
+            console.log(errors);
         }
     }
-    string(string) {
+    async string(string) {
         const regExp = this.validateRegExp('[^A-Za-z0-9]+');
         if (regExp.test(string)) {
             console.log('string validation failded');
             return false;
         };
     }
-    integer(integer) {
+    async integer(integer) {
         const regExp = this.validateRegExp('[^0-9]');
         if (regExp.test(`${integer}`)) {
             console.log('integer validation failded');
             return false;
-
         };
     }
-    min(amount, value) {
+    async min(amount, value) {
         let intValue = parseInt(amount);
         if (typeof value === 'string') {
             if (value.length >= intValue) console.log('minimum');
         }
     }
-    max(amount, value) {
+    async max(amount, value) {
         let intValue = parseInt(amount);
         if (typeof value === 'string') {
             if (value.length >= intValue) console.log('maximum length exceeded');
         }
     }
-    required(value, key) {
+    async required(value, key) {
         if (!Object.keys(this.request).includes(key) || this.request[key] == '')
             console.log(`${key} required`);
     }
-    json(value) {
+    async json(value) {
         try {
             JSON.parse(value);
         } catch (e) {
             console.log(e.message);
         }
     }
-    email(email) {
+    async email(email) {
         var emailRegex = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([\.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i;
         if (!emailRegex.test(email)) {
             console.log('email validation failded');
