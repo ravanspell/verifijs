53b534e7a2ae1e5825a79349b0d75cd364c80fa8
Ireshan Pathirana <Ireshan_107185@dialog.dialoggsm.com>
2020-04-19 14:35:12 +0530

    new version and readme.md update

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# js-validation
+# express-validation
 
-Agilent and light weight java script validation library for dummies.
+Agilent and light weight java script/node js validation library.
 
 ### Installation
 
-js-validation still under development thus installation is not possible so far.
+express-validation still under development thus installation is not possible so far as a npm library. But you can clone this repository and use it anyway.Try it is open source.
 
 ## Contributing
 
@@ -12,62 +12,107 @@ Pull requests are welcome. For major changes, please open an issue first to disc
 
 Please make sure to update tests as appropriate.
 
+## Why express-validation?
+
+express-validation is aligant validation library that inpired by laravel freamwork.
+
+- Express validation is very light weight validation library.
+- Express validation use very limited number of dependencies. (It is useig database driver packges such as mysql,mongodb,postgresql only).
+- Express validation define validation enable defined chained validation rule schema.It allow use multiple validations to single input.
+
+- It allow use even database based validations. For instance : If you want to check some data wether already in the database or not express validation allow check value by simply define a schema.
+- It currantly support only mongodb database validations. But as soon as possible it will support other database manamagent systems as well.
+
 ## Proposed Usage
 
-js-validation is light weight java script validation library It can be use with Node js, express java script server less (AWS,Azure, etc).
+express-validation is light weight java script/Node js validation library It can be use with Node js, express java script server less (AWS,Azure, etc).
 
 ```node
-const Validation = require('./Validation');
-
+const Validation = require("./express-validation");
+// the request object that include data which submitted by the user.
 let reqObj = {
-     name: 'kalo',
-    age: 45,
-    email 'myemail@gmail.com',
-    password: 'abc123',
-    location: "{'lat':23.454545,'lgt':45.565565}",
-}
+  name: "kalo",
+  age: 56,
+  address: "No234Knnimahara",
+  obj: '{"name":"kalo"}',
+  email: "ireshandj2@gmail.com",
+  id: "f1277d16-5cb9-43f6-95ce-a5e22e12cdaa",
+  is_enable: true,
+  term: "Yes",
+};
 
+//express-validation allows to defined your own validation error messages.
 const messages = {
-    name_required: "This porparty is required"
-}
+  first_name_required: "First name is required",
+  is_enable_boolean: "is enable must be true or false",
+  name_required: "This porparty is required",
+  name_alpha: "Name must be include letters and numbers",
+};
+//create validation object.
 const validation = new Validation();
 
-let status = validation.check(reqObj, {
-    name: 'required|string|max:2',
-    age: 'required|integer',
-    address: 'required|string',
-    obj: 'required|json',
-    email: 'required|email'
-}, messages);
+//------------database validation -----------
+
+//With creation validation object you can pass your database connection with //database type such as 'mongodb' or 'mysql' or etc.
+// const validation = new Validation(conn,'mongodb');
+//then express validation now ready to validate with database as well.
+
+//defined validation rules. This is asynchronous method.
+validation
+  .check(
+    reqObj,
+    {
+      first_name: "required|string|max:30|regExp:[a-zA-Z ]",
+      name: "required|string|max:30|regExp:[a-zA-Z ]|alpha",
+      age: "required|integer",
+      address: "required|string",
+      obj: "required|json",
+      email: "required|email",
+      id: "uuid",
+      is_enable: "boolean",
+      term: "in:yes,no,maybe",
+    },
+    messages
+  )
+  .then((result) => {
+    console.log(result);
+  });
 ```
 
 Above example shows the proposed example usage of js-validation.
 
 ```node
-const Validation = require("./Validation");
+const Validation = require("./express-validation");
 ```
 
 import validation library 'the standerd way'
 
 ```node
 let reqObj = {
-    name: 'kalo',
-    age: 45,
-    email 'myemail@gmail.com',
-    password: 'abc123',
-    location: "{'lat':23.454545,'lgt':45.565565}",
-}
+  name: "kalo",
+  age: 56,
+  address: "No234Knnimahara",
+  obj: '{"name":"kalo"}',
+  email: "ireshandj2@gmail.com",
+  id: "f1277d16-5cb9-43f6-95ce-a5e22e12cdaa",
+  is_enable: true,
+  term: "Yes",
+};
 ```
 
 'reqObj' is the object that include values that need to validate. this may be a 'post' request or your own reqObj.
 
 ```node
 {
-  name: 'required|string|min:1|max:20',
-  age: 'required|integer',
-  email: 'required|email',
-  password: 'required|min:8|max:30',
-  location: 'json'
+    first_name: 'required|string|max:30|regExp:[a-zA-Z ]',
+    name: 'required|string|max:30|regExp:[a-zA-Z ]|alpha',
+    age: 'required|integer',
+    address: 'required|string',
+    obj: 'required|json',
+    email: 'required|email',
+    id: 'uuid',
+    is_enable: 'boolean',
+    term: 'in:yes,no,maybe',
 }
 ```
 
@@ -83,7 +128,7 @@ pass the message object into check method just like above.here you have tow opti
 ```node
 const messages = {
   // max is the criteria
-  max: "you cannot use more than max value"
+  max: "you cannot use more than max value",
 };
 ```
 
@@ -93,7 +138,7 @@ this messages will apply all "max" validation checks.
 
 ```node
 const messages = {
-  name_required: "This property is required"
+  name_required: "This property is required",
 };
 ```
 
@@ -101,4 +146,4 @@ here you need to specify data field (name) and criteria (required) then error
 message will add only to that specific data field required criteria. data field and
 criteria should separate by using "\_".
 
-**This sample usage not only show all the validation parameters.**
+**This sample usage not only show all the validation parameters. We are working on full documentation.**
--- Validation.js
+++ Validation.js
@@ -1,156 +1,42 @@
-/**
- * Author: Ireshan M Pathirana 
- * Licence: GNU Public Licence
- * Usage: any java script framework 
- */
-const Util = require('./util');
-const dbFactory = require('./dbFactory');
-class Validation {
-    /**
-     * 
-     * @param {string} dbConnection dbConnection optional parameater
-     * @param {string} dbType 
-     * @param {string} dbName 
-     */
-    constructor(dbConnection = null, dbType = null) {
-        Object.assign(this, dbFactory.InitDbService(dbType, dbConnection));
-    }
-    async check(request, checkObj, messages = {}) {
-        let errorArry = [];
-        for (const property in checkObj) {
-            let check = checkObj[property].split('|');
-            for (let type of check) {
-                if (type.includes(':')) {
-                    let [func, amount] = type.split(':');
-                    let customMessage = this.messageProcessor(messages, property, func);
-                    let validationState = await this[`${func}Validation`](amount, request[property], customMessage, property);
-                    if (!validationState.status)
-                        errorArry.push(validationState.message);
-                }
-                if (!type.includes(':')) {
-                    let customMessage = this.messageProcessor(messages, property, type);
-                    let validationState = await this[`${type}Validation`](request, property, customMessage);
-                    if (!validationState.status)
-                        errorArry.push(validationState.message);
-                }
-            }
-        }
-        return { validation: errorArry.length > 0 ? false : true, error: errorArry }
-    }
+const Validation = require('.');
 
-    async stringValidation(request, property, customMessage) {
-        if (!(typeof request[property] === 'string')) {
-            let defaultErrorMessage = "Error: Invalid string";
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        };
-        return { status: true };
-    }
-    async alphaValidation(request, property, customMessage) {
-        const regExp = this.validateRegExp('[^A-Za-z0-9 ]+');
-        if (regExp.test(request[property])) {
-            let defaultErrorMessage = "Error: Special characters included";
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        };
-        return { status: true };
-    }
-    async integerValidation(request, property, customMessage) {
-        const regExp = this.validateRegExp('[^0-9]');
-        if (regExp.test(`${request[property]}`)) {
-            let defaultErrorMessage = "Error: Invalid integer";
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        };
-        return { status: true };
-    }
-    async minValidation(amount, value, customMessage) {
-        let intValue = parseInt(amount);
-        if (typeof value === 'string') {
-            if (value.length < intValue) {
-                let defaultErrorMessage = `Error: Minimum string length is ${intValue}`;
-                return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-            }
-        }
-        return { status: true };
-    }
-    async maxValidation(amount, value, customMessage) {
-        console.log(`amount is ${amount} value is ${value}`);
-        let intValue = parseInt(amount);
-        if (typeof value === 'string') {
-            if (value.length >= intValue) {
-                let defaultErrorMessage = `Error: Maximum string length is ${intValue}`;
-                return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-            };
-        }
-        return { status: true };
-    }
-    async inValidation(setOfTerms, value, customMessage) {
-        if (value != undefined) {
-            let termsArray = setOfTerms.toLowerCase().split(',')
-            if (!termsArray.includes(value.toLowerCase())) {
-                let defaultErrorMessage = `Error: ${value} is not expect`;
-                return this.validationErrorInjector(defaultErrorMessage, customMessage);
-            }
-        }
-        return { status: true };
-    }
-
-    async requiredValidation(request, property, customMessage) {
-        if (!Object.keys(request).includes(property) || request[property] == '') {
-            let defaultErrorMessage = `Error: ${property} is required`;
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        }
-        return { status: true };
-    }
-    async jsonValidation(request, property, customMessage) {
-        try {
-            let jsontext = JSON.stringify(request[property]);
-            JSON.parse(jsontext);
-            return { status: true };
-        } catch (e) {
-            let defaultErrorMessage = "Error: Invalid json string";
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        }
-    }
-    async emailValidation(request, property, customMessage) {
-        var emailRegex = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([\.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i;
-        if (!emailRegex.test(request[property])) {
-            let defaultErrorMessage = "Error: Invalid email";
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        };
-        return { status: true };
-    }
-    async uuidValidation(request, property, customMessage) {
-        const uuidRegExp = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
-        if (!uuidRegExp.test(request[property])) {
-            let defaultErrorMessage = "Error: Invalid uuid";
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        };
-        return { status: true };
-    }
-    async booleanValidation(request, property, customMessage) {
-        let validationValue = request[property];
-        console.log('is enable value is', !validationValue == true);
-        if (!(validationValue === 1 || validationValue === 0 || validationValue === true || validationValue === false)) {
-            let defaultErrorMessage = "Error: Invalid boolean value";
-            return this.validationErrorInjector(defaultErrorMessage, customMessage);
-        };
-        return { status: true };
-    }
-    async regExpValidation(regExpression, value, customMessage) {
-        const regExp = this.validateRegExp(regExpression);
-        if (!regExp.test(value)) {
-            let defaultErrorMessage = "Error: Invalid input";
-            return Util.validationErrorInjector(defaultErrorMessage, customMessage);
-        };
-        return { status: true };
-    }
-    //Process custom error messages which user defined
-    messageProcessor(messages, property, type) {
-        let errorMessageField = Object.keys(messages).filter(messageKey => messageKey == `${property}_${type}` || messageKey == type);
-        return messages[errorMessageField];
-    }
-    //Returns regular expression object for given reg exp string
-    validateRegExp(regexp) {
-        return new RegExp(regexp);
-    }
+let reqObj = {
+    name: "Ical survey 2019",
+    first_name: "ireshan",
+    age: 56,
+    address: 'No234Knnimahara',
+    obj: '{"name":"kalo"}',
+    email: 'ireshandj2@gmail.com',
+    id: 'f1277d16-5cb9-43f6-95ce-a5e22e12cdaa',
+    is_enable: true,
+    term: 'Yes'
 }
-module.exports = Validation;
+const messages = {
+    first_name_required: "First name is required",
+    is_enable_boolean: "is enable must be true or false",
+    name_required: "This porparty is required",
+    name_alpha: "Name must be include letters and numbers"
+    // _id_unique: "this is not uniue try another one"
+}
+
+const validation = new Validation();
+
+validation.check(reqObj, {
+    first_name: 'required|string|max:30|regExp:[a-zA-Z ]',
+    name: 'required|string|max:30|regExp:[a-zA-Z ]|alpha',
+    age: 'required|integer',
+    address: 'required|string',
+    obj: 'required|json',
+    email: 'required|email',
+    id: 'uuid',
+    is_enable: 'boolean',
+    term: 'in:yes,no,maybe',
+    //_id: 'unique:status'
+}, messages).then(result => {
+    console.log(result);
+});
+
+
+
+
+
