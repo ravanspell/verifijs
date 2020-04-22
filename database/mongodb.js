a427e39a4f5d371080b9e618fc67e1c7fc41cdfd
Ireshan Pathirana <Ireshan_107185@dialog.dialoggsm.com>
2020-04-22 14:46:08 +0530

    #1 mongo db validation exists and unique

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,33 +1,48 @@
 const MongoClient = require('mongodb').MongoClient;
 const Util = require('../util');
 class MongoDb {
-    constructor(dbConnection) {
-        //this.connectionString = connectionString;
-        //this.database = database;
-        this.dbConnection = dbConnection;
+    constructor(connectionSettings) {
+        this.connectionSettings = connectionSettings;
+        this.dbConnection = null;
         this.uniqueValidation = this.uniqueValidation.bind(this);
+        this.existsValidation = this.existsValidation.bind(this);
     }
-    async connection(url, database) {
-        if (this.dbConnection == null) {
-            try {
-                const Client = new MongoClient(url, { useUnifiedTopology: true });
-                // Use connect method to connect to the Server
-                const client = await Client.connect();
-                console.info("mongo db connected");
-                return client.db(database);
-            } catch (error) {
-                console.log(error.message);
-            }
+    async connection() {
+        try {
+            const Client = new MongoClient(this.connectionSettings.url, { useUnifiedTopology: true });
+            // Use connect method to connect to the Server
+            const client = await Client.connect();
+            console.info("mongo db connected");
+            return client.db();
+        } catch (error) {
+            console.log(error.message);
         }
-        return this.dbConnection;
     }
     async uniqueValidation(dbCollection, userInputValue, customMessage, columnName) {
         try {
-            //this.dbConnection = await this.connection(this.connectionString, this.database);
+            if (this.dbConnection == null)
+                this.dbConnection = await this.connection();
             const collection = this.dbConnection.collection(dbCollection);
             let findings = await collection.find({ [columnName]: userInputValue }).toArray();
             if (findings.length > 0) {
                 let defaultErrorMessage = `Error: ${userInputValue} is not unique`;
+                return Util.validationErrorInjector(defaultErrorMessage, customMessage)
+            }
+            return { status: true };
+        } catch (error) {
+            console.log(error.message);
+            return { status: true };
+        }
+    }
+
+    async existsValidation(dbCollection, userInputValue, customMessage, columnName) {
+        try {
+            if (this.dbConnection == null)
+                this.dbConnection = await this.connection();
+            const collection = this.dbConnection.collection(dbCollection);
+            let findings = await collection.find({ [columnName]: userInputValue }).toArray();
+            if (!findings.length > 0) {
+                let defaultErrorMessage = `Error: ${userInputValue} is not exists`;
                 return Util.validationErrorInjector(defaultErrorMessage, customMessage)
             }
             return { status: true };
